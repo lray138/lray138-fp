f0f37662272df29538cd401e2e6b95944a81cf7e
lray138 <57813289+lray138@users.noreply.github.com>
2023-07-26 21:20:57 -0400

    Initial Bootstrap example

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     plugins: [
         new HtmlWebpackPlugin(),
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
 };
