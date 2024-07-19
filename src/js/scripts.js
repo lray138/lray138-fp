6ca549a55065448ca2c47a2d610b35e0cb9b0778
lray138 <57813289+lray138@users.noreply.github.com>
2024-07-19 11:14:56 -0400

    removing Quill, added surreal.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,13 @@
 import 'bootstrap';
 
+// HTMX
 import 'htmx.org';
 window.htmx = require('htmx.org');
 
+// Ramda
 import * as R from "ramda"
 R.identity();
+
+// import './quill';
+
+import "./surreal.js";
